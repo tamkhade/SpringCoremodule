8a36c794ac2d4646ef8568f6dcf2854b0ae5a112
Tamkhade21@gmail.com <Tamkhade@Mahesh-PC>
2018-01-30 11:11:29 +0530

    using constructor

---
+++ src/main/java/com/constructorlist/QuestionAns.java
@@ -0,0 +1,37 @@
+package com.constructorlist;
+import java.util.Iterator;
+import java.util.List;
+/**
+ * @author Tamkhade
+ * using constructor and list display question answer
+ * here we use iteraror to dispaly the info in console
+ */
+public class QuestionAns {
+	
+	
+	int id;
+	String question;
+	List<String> Answer;
+	public QuestionAns(int id,String question,List<String> ans) {
+		// TODO Auto-generated constructor stub
+		
+		this.id=id;
+		this.question=question;
+		this.Answer=ans;	
+	}
+	
+	void display()
+	{
+		System.out.println(question);
+	    System.out.println("answers are:"); 
+		Iterator<String> l=Answer.iterator();
+		while(l.hasNext())
+		{
+			System.out.println(l.next());
+		}
+		
+	}
+	
+	
+
+}
---
+++ src/main/java/com/constructorlist/Questiontest.java
@@ -0,0 +1,18 @@
+package com.constructorlist;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Questiontest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Resource r=new ClassPathResource("Applicationcontext.xml");
+		BeanFactory app=new XmlBeanFactory(r);
+		 QuestionAns q=(QuestionAns) app.getBean("list");
+		 q.display();
+	}
+
+}
